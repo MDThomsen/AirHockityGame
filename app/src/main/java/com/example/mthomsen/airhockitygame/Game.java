6f20b8c3cd8b72d12a955720f210dfce494547ba
MFHolm <maritafholm@gmail.com>
2015-06-17 15:36:37 +0200

    player klassen

--- app/src/main/java/com/example/mthomsen/airhockitygame/Game.java
+++ app/src/main/java/com/example/mthomsen/airhockitygame/Game.java
@@ -5,8 +5,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
+
+import java.lang.reflect.Array;
 
 /**
  * Created by MaritaHolm on 17/06/15.
@@ -16,10 +19,11 @@ public class Game extends Activity{
     private RelativeLayout mFrame;
     private Bitmap mBitmap1;
     private Bitmap mBitmap2;
-    private View player1;
-    private View player2;
+    private Player player1;
+    private Player player2;
     private int pointsToWin = 10;
     private static final String TAG = "AirHockity-tag";
+    private Player[] players;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,11 +36,14 @@ public class Game extends Activity{
         // Set up user interface
         mFrame = (RelativeLayout) findViewById(R.id.frame);
 
-        mBitmap1 = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
-        mBitmap2 = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
+        players = new Player[2];
+        mBitmap1 = BitmapFactory.decodeResource(getResources(), R.drawable.player1);
+        mBitmap2 = BitmapFactory.decodeResource(getResources(), R.drawable.player2);
         player1 = new Player(getApplicationContext(), 100,100, mBitmap1);
+        players[0]=player1;
         mFrame.addView(player1);
-        player2 = new Player(getApplicationContext(), 100, 1000, mBitmap2);
+        player2 = new Player(getApplicationContext(), 100, 800, mBitmap2);
+        players[1]=player2;
         mFrame.addView(player2);
         Log.d(TAG, "height" + mFrame.getBottom());
         Log.d(TAG,"width" + mFrame.getWidth());
@@ -47,6 +54,23 @@ public class Game extends Activity{
 
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (intersects(event.getRawX(), event.getRawY())) {
+
+        }
+        return true;
+
+    }
+
+    private boolean intersects(float x, float y) {
+        for (Player p: players) {
+            if (p.intersects(x,y)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 }
